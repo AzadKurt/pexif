5c1b2face9893a83686d7f9c3eb30cd76ae1ee9b
AzadKurt <56046250+AzadKurt@users.noreply.github.com>
2022-01-03 16:25:55 +0100

    refine method comments

--- src/helper.h
+++ src/helper.h
@@ -17,14 +17,16 @@ int last_index_of(char toFind, char *src);
 
 /**
  * Returns the byte position of the beginning of the exif marker 0xFFE1, i.e.
- * the byte position of 0xFF. Returns 0 if the image constitued by the bytes
+ * the byte position of 0xFF. Returns 0 if the image constituted by the bytes
  * array does not contain an exif marker.
  */
 long validate_exif_sig(long byte_count, BYTE img_bytes[]);
 
 /**
  * Given a file name file_name of an image that has been pexifed, returns
- * "pexif_" + file_name.
+ * "pexif_" + file_name. For file names that are absolute paths, "pexif_" is
+ * concatenated to the front of the processed file like this: "C:/image.jpg" ->
+ * "C:/pexif_image.jpg".
  */
 char *get_pexifed_file_name(char *file_name);
 
